8f386fdd88e5bda149905ff409df757baa361c35
furugenict8 <furugenict8@gmail.com>
2020-07-14 23:58:06 +0900

    AddFirebase_book_list_page2_add_comment

--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -4,28 +4,24 @@ import 'package:flutter/material.dart';
 class BookList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //buildについて調べよう
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('firebaseテスト'),
       ),
       body: StreamBuilder<QuerySnapshot>(
+        //stream StreamBuilderにまずstreamを組み込む　snapshot() はその時点のデータ（状態）を丸ごと読み込むもの。
         stream: Firestore.instance.collection('books').snapshots(),
+        //builder アプリの当該部分の外観を宣言する
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return new Text('Loading...');
-            default:
-              return new ListView(
-                children:
-                    snapshot.data.documents.map((DocumentSnapshot document) {
-                  return new ListTile(
-                    title: new Text(document['title']),
-                  );
-                }).toList(),
+          return ListView(
+            children: snapshot.data.documents.map((DocumentSnapshot document) {
+              return ListTile(
+                title: Text(document['title']),
               );
-          }
+            }).toList(),
+          );
         },
       ),
     );
